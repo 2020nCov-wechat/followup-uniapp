17f984cc847c5a5146771cdddeb5efd2d73e6804
shenkecoco <824459715@qq.com>
2020-02-19 19:51:11 +0800

    加入柱状图

--- pages/user/my.vue
+++ pages/user/my.vue
@@ -35,6 +35,23 @@
 					</view>
 
 					<view class='myService'>
+						<view class='title acea-row row-middle'>结果展示</view>
+						<view class='serviceList acea-row row-middle'>
+
+							<view class="qiun-charts">
+								<!--#ifdef MP-ALIPAY -->
+								<canvas canvas-id="canvasColumn" id="canvasColumn" class="charts" :width="cWidth*pixelRatio" :height="cHeight*pixelRatio"
+								 :style="{'width':cWidth+'px','height':cHeight+'px'}" @touchstart="touchColumn"></canvas>
+								<!--#endif-->
+								<!--#ifndef MP-ALIPAY -->
+								<canvas canvas-id="canvasColumn" id="canvasColumn" class="charts" @touchstart="touchColumn"></canvas>
+								<!--#endif-->
+							</view>
+
+						</view>
+					</view>
+
+					<view class='myService'>
 						<view class='title acea-row row-middle'>综合评价</view>
 						<view class='serviceList acea-row row-middle'>
 
@@ -106,15 +123,40 @@
 </template>
 
 <script>
+	import uCharts from '../../components/u-charts/u-charts.js';
+	var _self;
+	var canvaColumn = null;
 	export default {
 		data() {
 			return {
 				modalName: null,
 				imgPath: '../../static/imgs/result/noResult.png',
-				suggestion: '早睡早起，吃好喝好。'
+				suggestion: '早睡早起，吃好喝好。',
+				cWidth: '',
+				cHeight: '',
+				pixelRatio: 1,
+				textarea: ''
 			};
 		},
+		onLoad() {
+			_self = this;
+			//#ifdef MP-ALIPAY
+			uni.getSystemInfo({
+				success: function(res) {
+					if (res.pixelRatio > 1) {
+						//正常这里给2就行，如果pixelRatio=3性能会降低一点
+						//_self.pixelRatio =res.pixelRatio;
+						_self.pixelRatio = 2;
+					}
+				}
+			});
+			//#endif
+			this.cWidth = uni.upx2px(700);
+			this.cHeight = uni.upx2px(500);
+			this.getServerData();
+		},
 		methods: {
+
 			toLogin(){
 				uni.navigateTo({
 					url:'login'
@@ -144,13 +186,135 @@
 			tabSelect(e) {
 				this.TabCur = e.currentTarget.dataset.id;
 				this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60
-			}
-		},
-	}
+			},
+			getServerData() {
+				uni.request({
+							url: 'http://jsonplaceholder.typicode.com/users',
+							data: {},
+							success: function(res) {
+								console.log(res.data)
+								let Column = {
+									"categories": [],
+									"series": [{
+									  "name": "",
+									  "data": []
+									}]
+								};
+								//这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
+								// Column.categories = res.data.data.ColumnB.categories;
+								// Column.series = res.data.data.ColumnB.series;
+								if (res != null) {
+									for (var i = 0; i < 6; i++) {
+										
+										Column.categories.push(res.data[i].name); //挨个取出类别并填入类别数组
+										Column.series[0].data.push(res.data[i].id); //挨个取出销量并填入销量数组
+									}
+									//Column.series=[35,8,25,37,4,20];[0].name="得分"
+									Column.series[0].name="得分";
+				                }
+
+									console.log(Column.series);
+									//_self.textarea = JSON.stringify(res.data.data.ColumnB);
+									_self.showColumn("canvasColumn", Column);
+								},
+								fail: () => {
+									_self.tips = "网络错误，小程序端请检查合法域名";
+								},
+							});
+					},
+					showColumn(canvasId, chartData) {
+						canvaColumn = new uCharts({
+							$this: _self,
+							canvasId: canvasId,
+							type: 'column',
+							padding: [15, 5, 0, 15],
+							legend: {
+								show: true,
+								padding: 5,
+								lineHeight: 11,
+								margin: 0,
+							},
+							fontSize: 11,
+							background: '#FFFFFF',
+							pixelRatio: _self.pixelRatio,
+							animation: true,
+							categories: chartData.categories,
+							series: chartData.series,
+							xAxis: {
+								disableGrid: true,
+						        rotateLabel: true
+							},
+							yAxis: {
+								data: [{
+									position: 'left',
+									axisLine: false,
+									format: (val) => {
+										return val.toFixed(0) + '分'
+									},
+									min:0,
+									
+								}]
+							},
+							dataLabel: true,
+							width: _self.cWidth * _self.pixelRatio,
+							height: _self.cHeight * _self.pixelRatio,
+							extra: {
+								column: {
+									type: 'group',
+									width: _self.cWidth * _self.pixelRatio * 0.45 / chartData.categories.length
+								}
+							}
+						});
+
+					},
+					touchColumn(e) {
+						canvaColumn.showToolTip(e, {
+							format: function(item, category) {
+								if (typeof item.data === 'object') {
+									return category + ' ' + item.name + ':' + item.data.value
+								} else {
+									return category + ' ' + item.name + ':' + item.data
+								}
+							}
+						});
+						// canvaColumn.touchLegend(e, {
+						// 	animation: true
+						// });
+					},
+					changeData() {
+						if (isJSON(_self.textarea)) {
+							let newdata = JSON.parse(_self.textarea);
+							canvaColumn.updateData({
+								series: newdata.series,
+								categories: newdata.categories,
+								animation: true
+							});
+						} else {
+							uni.showToast({
+								title: '数据格式错误',
+								image: '../../../static/images/alert-warning.png'
+							})
+						}
+					}
+			},
+		}
 </script>
 
 <style>
 	@import "../../common/common.css";
+
+	/*样式的width和height一定要与定义的cWidth和cHeight相对应*/
+	.qiun-charts {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+
+	.charts {
+		width: 700upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
 
 	page {
 		background-image: var(--gradualBlue);
